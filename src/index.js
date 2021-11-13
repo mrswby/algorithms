34a1c41c174ecd8ecd31d3dcc8826ab9d09bd98f
Bei <myroslav.bei.pz.2020@lpnu.ua>
2021-11-13 18:09:48 +0200

    refactor: add ChakraProvider to the App component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import {ChakraProvider} from "@chakra-ui/react"
+
+const App = () => {
+  return (
+    null
+  );
+}
 
 ReactDOM.render(
-  <React.StrictMode><App/></React.StrictMode>,
+  <React.StrictMode>
+    <ChakraProvider>
+      <App/>
+    </ChakraProvider>
+  </React.StrictMode>,
   document.getElementById('root')
 );
